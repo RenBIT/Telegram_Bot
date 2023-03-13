8c49616c86b7cbcf14b2952d23d417ce94644895
Renat <35333158+RenBIT@users.noreply.github.com>
2023-03-13 14:40:10 +0300

    Update Token.go

--- Telegram_Bot/Token.go
+++ Telegram_Bot/Token.go
@@ -1,8 +1,10 @@
+/*
+Можно удалить!! -просто получает токен из файла!
+*/
 package main
 
 import "io/ioutil"
 
-//Можно удалить!! - только для разработки
 func Token() string {
 	Token, err := ioutil.ReadFile("../../taboo/Token.txt")
 	if err != nil {
